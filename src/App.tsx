08dfad1e650d1234590f1c6dcf73753449316963
Master God <gurrakhakhatri@gmail.com>
2024-12-03 14:24:02 +0530

    added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
 import "./App.css"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen w-screen">
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/tasks" />} />
+      <Route index={true} path="/tasks" element={<div className="flex items-center justify-center h-screen w-screen">
       <div className="bg-white flex flex-col gap-5 rounded-lg w-full h-full sm:w-[90%] sm:h-[99%] md:h-[90%] xl:w-2/3 2xl:w-1/2 p-5 overflow-hidden">
         <h1 className="text-4xl text-center font-medium">Task Manager</h1>
         <TaskForm />
         <TaskList />
       </div>
-    </div>
+    </div>} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
